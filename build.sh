9bc3257fa641ca2917af5daf8ee0e3fdc9956f8f
Chu <tomzz233333@gmail.com>
2021-11-11 21:19:49 +0800

    add new function

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # require password of database to execute this script
 if [ $# -lt "2" ]; then
-	echo "usage: build.sh <password> scheme|database|backup"
+	echo "usage: build.sh <password> scheme|database|backup|new-scheme"
 	exit 1
 fi
 passwd=$1
@@ -19,8 +19,13 @@ build_database () {
 	awk -f sql_insert.awk $data_files | mysql -p$passwd mitocw
 }
 
+build_backup () {
+	mysqldump -p$passwd mitocw > mitocw_scheme.sql
+}
+
 case $2 in
 	"scheme") build_scheme ;;
 	"database") build_database ;;
-	"backup") mysqldump -p$passwd mitocw > mitocw_scheme.sql;;
+	"backup") build_backup ;;
+	"new-scheme") build_scheme && mysql -p$passwd && build_backup ;;
 esac
